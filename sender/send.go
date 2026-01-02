730671c4b06b3aef1da3c56b711094a2a619d41a
KhoaND <khoanguyenduc99@gmail.com>
2026-01-02 16:37:36 +0700

    feat: add RabbitMQ new task sender supporting persistent messages and command-line arguments.

--- sender/send.go
+++ sender/send.go
@@ -1,54 +1,52 @@
-package main
+// package main
 
-import (
-	"context"
-	"log"
-	"time"
+// import (
+// 	"context"
+// 	"log"
+// 	"time"
 
-	amqp "github.com/rabbitmq/amqp091-go"
-)
+// 	amqp "github.com/rabbitmq/amqp091-go"
+// )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
+// func failOnError(err error, msg string) {
+// 	if err != nil {
+// 		log.Panicf("%s: %s", msg, err)
+// 	}
+// }
 
-func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+// func main() {
+// 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+// 	failOnError(err, "Failed to connect to RabbitMQ")
+// 	defer conn.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+// 	ch, err := conn.Channel()
+// 	failOnError(err, "Failed to open a channel")
+// 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+// 	q, err := ch.QueueDeclare(
+// 		"hello", // name
+// 		false,   // durable
+// 		false,   // delete when unused
+// 		false,   // exclusive
+// 		false,   // no-wait
+// 		nil,     // arguments
+// 	)
+// 	failOnError(err, "Failed to declare a queue")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+// 	defer cancel()
 
-	body := "Hello World!"
-	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		},
-	)
-	failOnError(err, "Failed to publish a message")
-	log.Printf("[x] Sent %s\n", body)
-}
-
-
+// 	body := "Hello World!"
+// 	err = ch.PublishWithContext(ctx,
+// 		"",     // exchange
+// 		q.Name, // routing key
+// 		false,  // mandatory
+// 		false,  // immediate
+// 		amqp.Publishing{
+// 			ContentType: "text/plain",
+// 			Body:        []byte(body),
+// 		},
+// 	)
+// 	failOnError(err, "Failed to publish a message")
+// 	log.Printf("[x] Sent %s\n", body)
+// }
